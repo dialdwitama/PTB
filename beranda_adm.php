add5861ec43396d1f207e8b1d3e3e9439cea4a7e
dialdwitama <118799563+dialdwitama@users.noreply.github.com>
2023-03-28 11:47:14 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,23 @@
+<div class="container">
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img width="90%" src="img/home.jpg"></img></p>
+                            <br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ menu.php
@@ -0,0 +1,36 @@
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="?page=utama">Sistem Informasi PTB LLDIKTI4</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+
+                <?php if(isset($_SESSION['level']) && $_SESSION['level']==1) { ?>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kelola Data <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="?page=arsip&actions=tampil">PTS Bermasalah</a></li>
+                        <li><a href="?page=peminjaman&actions=tampil">Rekapitulasi Monev</a></li>
+                    </ul>
+                </li>
+                <li><ul><a></a></li></ul>
+                <?php } ?>
+                    <?php if (isset($_SESSION['username'])) { ?>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php } ?>
+
+            </ul>
+        </div>
+    </div>
+</nav>
