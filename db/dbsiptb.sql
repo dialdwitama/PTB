9e996a75a239ae6af12b04c6f88178e5251bae12
Shafa Alya <shafaalya79@gmail.com>
2023-03-30 08:33:44 +0700

    ada

--- db/dbsiptb.sql
+++ db/dbsiptb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Mar 2023 pada 09.59
+-- Waktu pembuatan: 30 Mar 2023 pada 03.32
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -24,11 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `detail_monev`
+--
+
+CREATE TABLE `detail_monev` (
+  `id` int(5) NOT NULL,
+  `a_monev` varchar(100) NOT NULL,
+  `a_direk` varchar(200) NOT NULL,
+  `a_pddikti` varchar(200) NOT NULL,
+  `h_monev` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_monev`
+--
+
+INSERT INTO `detail_monev` (`id`, `a_monev`, `a_direk`, `a_pddikti`, `h_monev`) VALUES
+(1, 'Jl.PHH.Mustafa no.32', 'Jl. PHH.Mustafa no.32', 'Jl.Suci no.38', 'Lahan Perpustakaan Seluas 32,5 M2 (belum memenuhi)\r\nJudul buku sebanyak 136 (belum memenuhi)\r\nPermasalahan di SAPTO terkait pengajuan akreditasi prodi baru');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `monev`
 --
 
 CREATE TABLE `monev` (
-  `no` int(5) NOT NULL,
+  `id` int(5) NOT NULL,
   `kode` int(10) NOT NULL,
   `pt` varchar(100) NOT NULL,
   `tglmonev` date DEFAULT NULL,
@@ -39,7 +60,7 @@ CREATE TABLE `monev` (
 -- Dumping data untuk tabel `monev`
 --
 
-INSERT INTO `monev` (`no`, `kode`, `pt`, `tglmonev`, `masalah`) VALUES
+INSERT INTO `monev` (`id`, `kode`, `pt`, `tglmonev`, `masalah`) VALUES
 (1, 12121, 'Universitas Widyatama', '2023-03-08', '1.Lahan Perpustakaan Seluas 84 M2 (belum memenuhi)\r\n2.Judul buku sebanyak 162 (belum memenuhi)\r\n3.Program D3 Teknik Informatika di pantau BAN-PT karena Tidak Terakreditasi');
 
 -- --------------------------------------------------------
@@ -56,7 +77,7 @@ CREATE TABLE `pts` (
   `tglProgressTerakhir` date NOT NULL,
   `deadline` date NOT NULL,
   `stat` varchar(50) NOT NULL,
-  `perhitungan` int(10) NOT NULL,
+  `progressReport` int(10) NOT NULL,
   `pic` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -64,8 +85,8 @@ CREATE TABLE `pts` (
 -- Dumping data untuk tabel `pts`
 --
 
-INSERT INTO `pts` (`id`, `kode`, `pt`, `progressTerakhir`, `tglProgressTerakhir`, `deadline`, `stat`, `perhitungan`, `pic`) VALUES
-(1, 43258, 'Sekolah Tinggi Ilmu Komunikasi Wangsa Jaya', 'Akan segera bergabung dengan STKIP Situs Banten serta pengajuan tutup pada program studi yang pembinaan', '2022-10-05', '2022-12-31', 'Menunggu pergabungan', 0, 'LLDIKTI');
+INSERT INTO `pts` (`id`, `kode`, `pt`, `progressTerakhir`, `tglProgressTerakhir`, `deadline`, `stat`, `progressReport`, `pic`) VALUES
+(1, 12121, 'Universitas Komputer Indonesia', 'Akan segera bergabung dengan STKIP Situs Banten serta pengajuan tutup pada program studi yang pembinaan\r\n', '2023-03-02', '2023-03-23', 'AMAN', 12, 'LLDIKTI4');
 
 -- --------------------------------------------------------
 
@@ -94,10 +115,16 @@ INSERT INTO `user` (`username`, `paswd`, `email`, `nama`, `level`, `ket`) VALUES
 --
 
 --
+-- Indeks untuk tabel `detail_monev`
+--
+ALTER TABLE `detail_monev`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `monev`
 --
 ALTER TABLE `monev`
-  ADD PRIMARY KEY (`no`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `pts`
@@ -119,7 +146,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `monev`
 --
 ALTER TABLE `monev`
-  MODIFY `no` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `pts`
