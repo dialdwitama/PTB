fa04961d5fd3b840985f2c1a073da8431bdfb003
Dial Dwitama <dialldwitama167@gmail.com>
2023-03-31 11:37:16 +0700

    PAK GANJAR

--- views/arsip_tambah.php
+++ views/arsip_tambah.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="panel panel-success">
-                <div class="panel-heading">
+                <div style="background: #1E90FF" class="panel-heading">
                     <h3 class="panel-title">Form Tambah Data PTB</h3>
                 </div>
                 <div class="panel-body">
